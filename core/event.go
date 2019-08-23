9b61d1b5cc0daa81dd83cd4c0081180afbcd27c8
Timothy <yexiaozhou2003@gmail.com>
2019-08-23 16:45:36 +0800

    refactor

--- core/event.go
+++ core/event.go
@@ -1,1 +1,47 @@
 package core
+
+import (
+	ui "github.com/gizak/termui/v3"
+)
+
+func updateParagraph(key string) {
+	p.Text = key
+}
+
+func setBorder() {
+	border = !border
+	p.Border = border
+}
+
+func HandleEvents() {
+	uiEvents := ui.PollEvents()
+	defer ui.Close()
+
+	for {
+		e := <-uiEvents
+		switch e.ID {
+		case "?":
+			// show help menu
+			updateParagraph("[↑]:j [↓]:k [b]:Boss Key [g]:Show/Hide Grid, [q]:Quit")
+			ui.Render(p)
+		case "p":
+			// show progress
+		case "f":
+			// show border
+			setBorder()
+			ui.Render(p)
+		case "b":
+			// boss key
+			p.Border = false
+			p.Text = "[root@localhost]$"
+			ui.Render(p)
+		case "q", "<C-c>":
+			// quit
+			return
+		case "j", "k", "<C-n>", "<C-p>":
+			// switch content
+			updateParagraph(e.ID)
+			ui.Render(p)
+		}
+	}
+}
--- core/widget.go
+++ core/widget.go
@@ -1,1 +1,33 @@
 package core
+
+import (
+	"fmt"
+	"log"
+
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+)
+
+var (
+	border = false
+	p      *widgets.Paragraph
+)
+
+func Init() {
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
+
+	width, height := ui.TerminalDimensions()
+
+	p = widgets.NewParagraph()
+	// p.Title = "Text Box"
+	p.Text = fmt.Sprintf("%d,%d", width, height)
+	p.SetRect(0, 0, width, 3)
+	p.TextStyle.Fg = ui.ColorWhite
+	p.BorderStyle.Fg = ui.ColorCyan
+	p.Border = border
+	// p.WrapText = true
+
+	ui.Render(p)
+}
